3cf435d56a9ad4268a536cf2614d7ea4db82fa62
nkz <5499916+nkz-soft@users.noreply.github.com>
2024-12-08 14:31:44 +0300

    100. Same Tree

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ LeetCode
 ### LeetCode Algorithm
 
 
-| # | Title | Solution                   | Difficulty |
-|-| ----- |----------------------------|------------|
-|1|[Two Sum](https://leetcode.com/problems/two-sum/) | [C#](./csharp/_1_Two_Sum/Solution.cs) | Easy |
+| #   | Title                                                 | Solution                                  | Difficulty |
+|-----|-------------------------------------------------------|-------------------------------------------|------------|
+| 1   | [Two Sum](https://leetcode.com/problems/two-sum/)     | [C#](./csharp/_1_Two_Sum/Solution.cs)     | Easy |
+| 100 | [Same Tree](https://leetcode.com/problems/same-tree/) | [C#](./csharp/_100_Same_Tree/Solution.cs) | Easy |
---
+++ csharp/_100_Same_Tree/Solution.cs
@@ -0,0 +1,81 @@
+﻿using Xunit;
+
+namespace Leetcode._100_Same_Tree;
+
+using System.Collections;
+using Common;
+using FluentAssertions;
+
+public class Solution
+{
+    public bool IsSameTree(TreeNode? p, TreeNode? q)
+    {
+        if (p is null && q is null)
+        {
+            return true;
+        }
+
+        if (p is null || q is null)
+        {
+            return false;
+        }
+
+        if (p.val != q.val)
+        {
+            return false;
+        }
+        return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+    }
+
+
+    [Theory]
+    [ClassData(typeof(IsSameTreeTestData))]
+    public void IsSameTreeTest(TreeNode p, TreeNode q, bool expected)
+    {
+        IsSameTree(p, q).Should().Be(expected);
+    }
+
+    class IsSameTreeTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return
+            [
+                new TreeNode(1,
+                    new TreeNode(2),
+                    new TreeNode(3)
+                    ),
+                new TreeNode(1,
+                    new TreeNode(2),
+                    new TreeNode(3)
+                ),
+                true
+            ];
+            yield return
+            [
+                new TreeNode(1,
+                    new TreeNode(2)
+                ),
+                new TreeNode(1,
+                    null,
+                    new TreeNode(2)
+                ),
+                false
+            ];
+            yield return
+            [
+                new TreeNode(1,
+                    new TreeNode(2),
+                    new TreeNode(1)
+                ),
+                new TreeNode(1,
+                    new TreeNode(1),
+                    new TreeNode(2)
+                ),
+                false
+            ];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
