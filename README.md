9640ce53a1df93f23cbef2d98bed480fb2b0940d
nkz <5499916+nkz-soft@users.noreply.github.com>
2024-12-12 14:43:25 +0300

    112. Path Sum

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ LeetCode
 ### LeetCode Algorithm
 
 
-| #   | Title                                                 | Solution                                  | Difficulty |
-|-----|-------------------------------------------------------|-------------------------------------------|------------|
-| 1   | [Two Sum](https://leetcode.com/problems/two-sum/)     | [C#](./csharp/_1_Two_Sum/Solution.cs)     | Easy |
+| #   | Title                                        | Solution                                  | Difficulty |
+|-----|----------------------------------------------|-------------------------------------------|------------|
+| 1   | [Two Sum](https://leetcode.com/problems/two-sum/) | [C#](./csharp/_1_Two_Sum/Solution.cs)     | Easy |
 | 100 | [Same Tree](https://leetcode.com/problems/same-tree/) | [C#](./csharp/_100_Same_Tree/Solution.cs) | Easy |
+| 112 | [Path Sum](https://leetcode.com/problems/path-sum/)  | [C#](./csharp/_112_Path_Sum/Solution.cs)  | Easy |
---
+++ csharp/_112_Path_Sum/Solution.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+
+namespace Leetcode._112_Path_Sum;
+
+using System.Collections;
+using Common;
+using FluentAssertions;
+
+public class Solution
+{
+    public bool HasPathSum(TreeNode? root, int targetSum)
+    {
+        if (root is null)
+        {
+            return false;
+        }
+
+        targetSum -= root.val;
+        if (targetSum == 0 && root.left is null && root.right is null)
+        {
+            return true;
+        }
+        return HasPathSum(root.left, targetSum) || HasPathSum(root.right, targetSum);
+    }
+
+    [Theory]
+    [ClassData(typeof(HasPathSumTestData))]
+    public void HasPathSumTest(TreeNode root, int targetSum, bool expected)
+    {
+        HasPathSum(root, targetSum).Should().Be(expected);
+    }
+
+    class HasPathSumTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return
+            [
+                new TreeNode(5,
+                    new TreeNode(4,
+                        new TreeNode(11,
+                            new TreeNode(7),
+                            new TreeNode(2))),
+                    new TreeNode(8,
+                        new TreeNode(13),
+                        new TreeNode(4,
+                            null,
+                            new TreeNode(1)))
+                    ),
+                22,
+                true
+            ];
+
+            yield return
+            [
+                new TreeNode(1,
+                    new TreeNode(2),
+                    new TreeNode(3)
+                    ),
+                5,
+                false
+            ];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
