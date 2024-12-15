f56a73af7bbe84793f9982297f1c338ce4b6a12a
nkz <5499916+nkz-soft@users.noreply.github.com>
2024-12-15 15:02:36 +0300

    304. Range Sum Query 2D - Immutable

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ LeetCode
 |-----|----------------------------------------------|-------------------------------------------|------------|
 | 1   | [Two Sum](https://leetcode.com/problems/two-sum/) | [C#](./csharp/_1_Two_Sum/Solution.cs)     | Easy |
 | 100 | [Same Tree](https://leetcode.com/problems/same-tree/) | [C#](./csharp/_100_Same_Tree/Solution.cs) | Easy |
-| 112 | [Path Sum](https://leetcode.com/problems/path-sum/)  | [C#](./csharp/_112_Path_Sum/Solution.cs)  | Easy |
+| 112 | [Path Sum](https://leetcode.com/problems/path-sum/) | [C#](./csharp/_112_Path_Sum/Solution.cs)  | Easy |
+| 304 | [Range Sum Query 2D - Immutable](https://leetcode.com/problems/range-sum-query-2d-immutable/)  | [C#](./csharp/_304_Range_Sum_Query_2D_Immutable/Solution.cs)  | Medium |
---
+++ csharp/_304_Range_Sum_Query_2D_Immutable/Solution.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+using Assert = Xunit.Assert;
+
+namespace Leetcode._304_Range_Sum_Query_2D_Immutable;
+
+public class Solution
+{
+    public class NumMatrix {
+
+        private readonly int[,] prefixSumMatrix;
+
+        public NumMatrix(int[][] matrix)
+        {
+            if (matrix.Length == 0 || matrix[0].Length == 0)
+            {
+                return;
+            }
+
+            var rows = matrix.Length;
+            var cols = matrix[0].Length;
+
+            prefixSumMatrix = new int[rows + 1, cols + 1];
+            for (var i = 1; i <= rows; ++i)
+            {
+                for (var j = 1; j <= cols; ++j)
+                {
+                    prefixSumMatrix[i, j] = prefixSumMatrix[i - 1, j]
+                                            + prefixSumMatrix[i, j - 1]
+                                            - prefixSumMatrix[i - 1, j - 1]
+                                            + matrix[i - 1][j - 1];
+                }
+            }
+        }
+
+        public int SumRegion(int row1, int col1, int row2, int col2)
+        {
+            return prefixSumMatrix[row2 + 1, col2 + 1]
+                   - prefixSumMatrix[row2 + 1, col1]
+                   - prefixSumMatrix[row1, col2 + 1]
+                   + prefixSumMatrix[row1, col1];
+        }
+    }
+
+    [Fact]
+    public void NumMatrixTest()
+    {
+        var obj = new NumMatrix(new int[][] { new int[] { 3, 0, 1, 4, 2 }, new int[] { 5, 6, 3, 2, 1 }, new int[] { 1, 2, 0, 1, 5 }, new int[] { 4, 1, 0, 1, 7 }, new int[] { 1, 0, 3, 0, 5 } });
+        var result = obj.SumRegion(2, 1, 4, 3);
+        Assert.Equal(8, result);
+    }
+}
