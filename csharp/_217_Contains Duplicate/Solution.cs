7e4d496a06737bad159f96f9b42bff15dccb7710
nkz <5499916+nkz-soft@users.noreply.github.com>
2025-01-03 11:48:02 +0300

    217. Contains Duplicate

---
+++ csharp/_217_Contains Duplicate/Solution.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace Leetcode._217_Contains_Duplicate;
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var seen = new HashSet<int>();
+        foreach (var num in nums) {
+            if (!seen.Add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    [Theory]
+    [InlineData(new[] {1,2,3,1}, true)]
+    [InlineData(new[] {1,2,3,4}, false)]
+    [InlineData(new[] {1,1,1,3,3,4,3,2,4,2}, true)]
+    public void ContainsDuplicateTest(int[] nums, bool expected)
+    {
+        var result = ContainsDuplicate(nums);
+        Assert.Equal(expected, result);
+    }
+}
