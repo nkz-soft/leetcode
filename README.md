e8081dff20089fd234a4632acd037cf8a6571cef
nkz <5499916+nkz-soft@users.noreply.github.com>
2025-01-05 15:24:01 +0300

    108. Convert Sorted Array to Binary Search Tree

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ LeetCode
 | 19  | [Remove Nth Node From End of List](https://leetcode.com/problems/remove-nth-node-from-end-of-list/) | [C#](./csharp/_19_Remove_Nth_Node_From_End_of_List/Solution.cs) | Medium |
 | 20  | [Valid Parentheses](https://leetcode.com/problems/valid-parentheses/) | [C#](./csharp/_20_Valid_Parentheses/Solution.cs)  | Easy |
 | 100 | [Same Tree](https://leetcode.com/problems/same-tree/) | [C#](./csharp/_100_Same_Tree/Solution.cs) | Easy |
+| 108 | [Convert Sorted Array to Binary Search Tree](https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/) | [C#](./csharp/_108_Convert_Sorted_Array_to_Binary_Search_Tree/Solution.cs) | Easy |
 | 112 | [Path Sum](https://leetcode.com/problems/path-sum/) | [C#](./csharp/_112_Path_Sum/Solution.cs) | Easy |
 | 217 | [Contains Duplicate](https://leetcode.com/problems/contains-duplicate/) | [C#](./csharp/_217_Contains Duplicate/Solution.cs) | Easy |
 | 304 | [Range Sum Query 2D - Immutable](https://leetcode.com/problems/range-sum-query-2d-immutable/) | [C#](./csharp/_304_Range_Sum_Query_2D_Immutable/Solution.cs) | Medium |
---
+++ csharp/_108_Convert_Sorted_Array_to_Binary_Search_Tree/Solution.cs
@@ -0,0 +1,56 @@
+﻿namespace Leetcode._108_Convert_Sorted_Array_to_Binary_Search_Tree;
+
+using System.Collections;
+using Common;
+using FluentAssertions;
+using Xunit;
+
+public class Solution
+{
+    public TreeNode? SortedArrayToBST(int[] nums)
+    {
+        var root = BuildTree(nums, 0, nums.Length - 1);
+        return root;
+    }
+
+    private TreeNode? BuildTree(int[] nums, int leftIndex, int rightIndex)
+    {
+        if (leftIndex > rightIndex)
+        {
+            return null;
+        }
+
+        var mid = (leftIndex + rightIndex) / 2;
+
+        var leftNode = BuildTree(nums, leftIndex, mid - 1);
+        var rightNode = BuildTree(nums, mid + 1, rightIndex);
+
+        return new TreeNode(nums[mid], leftNode, rightNode);
+    }
+
+    [Theory]
+    [ClassData(typeof(SortedArrayToBSTTestData))]
+    public void SortedArrayToBSTTest(int[] nums, TreeNode expected)
+    {
+        SortedArrayToBST(nums).Should().BeEquivalentTo(expected);
+    }
+
+    private class SortedArrayToBSTTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return
+            [
+                new[] {-10, -3, 0, 5, 9},
+                             new TreeNode(
+                                 0,
+                                 new TreeNode(-10, null, new TreeNode(-3)),
+                                 new TreeNode(5, null, new TreeNode(9))
+                                 ),
+            ];
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
+
+
